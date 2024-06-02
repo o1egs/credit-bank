e6ecad2aec952f8defa4fe0611505b8f239e2577
Олег <102363308+o1egs@users.noreply.github.com>
2024-06-02 13:58:57 +0400

    Revert "Feature/calculator service/init controller"

--- calculator-service/src/main/java/ru/shtyrev/calculator_service/enums/EmploymentStatus.java
+++
@@ -1,7 +0,0 @@
-package ru.shtyrev.calculator_service.enums;
-
-public enum EmploymentStatus {
-    UNEMPLOYED,
-    SELF_EMPLOYED,
-    BUSINESS_OWNER
-}
--- calculator-service/src/main/java/ru/shtyrev/calculator_service/enums/Position.java
+++
@@ -1,6 +0,0 @@
-package ru.shtyrev.calculator_service.enums;
-
-public enum Position {
-    MIDDLE_MANAGER,
-    TOP_MANAGER
-}
