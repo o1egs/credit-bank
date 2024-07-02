f7c3bab2c1c0ecdbf78ba86931aab19713007794
Олег <Tankistonline21@gmail.com>
2024-07-02 12:37:53 +0400

    add emailmessage dto and theme enum

---
+++ dtos/src/main/java/ru/shtyrev/dtos/enums/Theme.java
@@ -0,0 +1,10 @@
+package ru.shtyrev.dtos.enums;
+
+public enum Theme {
+    FINISH_REGISTRATION,
+    CREATE_DOCUMENTS,
+    SEND_DOCUMENTS,
+    SEND_SES,
+    CREDIT_ISSUED,
+    STATEMENT_DENIED;
+}
